f38497bdc4a3f7f3a61e9748192ffe6b5ec6eeca
Tenmayos <tenmayos_2@hotmail.com>
2023-05-19 17:02:04 +0300

    Initial commit with everything prefixed to start building Calculex

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Calculex;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		// MainPage behaves like the MainWindow rather than a page.
+		MainPage = new AppShell();
+		MainPage.Title = "Calculex";
+	}
+}
