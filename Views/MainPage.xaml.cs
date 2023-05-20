f65af08021b46a5eeffa3d7d00d62dcc9dd03e1a
Tenmayos <tenmayos_2@hotmail.com>
2023-05-20 05:26:14 +0300

    Added some more buttons in the

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -2,32 +2,18 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
-        btn.Focused += Btn_Focused;
-        btn.Unfocused += Btn_Unfocused;
-    }
-
-    private void Btn_Unfocused(object sender, FocusEventArgs e)
-    {
-		btn.BackgroundColor = Colors.Blue;
-    }
-
-    private void Btn_Focused(object sender, FocusEventArgs e)
-    {
-		btn.BackgroundColor = Colors.Red;
     }
 
     private void OnMouseEnter(object sender, EventArgs e)
 	{
-        btn.BackgroundColor = Colors.Red;
+        (sender as Button).Opacity = 0.8f;
 	}
     private void OnMouseExit(object sender, EventArgs e)
     {
-        btn.BackgroundColor = Colors.White;
+        (sender as Button).Opacity = 1;
     }
 }
 
