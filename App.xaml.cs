ca27f453d08dff5d829f164f3bc20ec80dddf277
Tenmayos <tenmayos_2@hotmail.com>
2023-05-25 12:39:24 +0300

    overrid the App's window size for windowsOS in

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,4 +11,15 @@ public partial class App : Application
 		MainPage = new AppShell();
 		MainPage.Title = "Calculex";
 	}
+
+	// If the OS is windows, we override the window creation.
+#if WINDOWS
+	protected override Window CreateWindow(IActivationState activationState)
+    {
+		Window mainAppWindow = base.CreateWindow(activationState);
+		mainAppWindow.Width = 500;
+		mainAppWindow.Height = 750;
+		return mainAppWindow;
+    }
+#endif
 }
