2bfbc28ad093271a99e1c9f24fbc6134a16e2cb8
Tenmayos <tenmayos_2@hotmail.com>
2023-05-29 15:30:04 +0300

    Decided to go with a new approach that uses lists to keep track of values and operators

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -5,10 +5,12 @@ namespace Calculex.Views;
 public partial class MainPage : ContentPage
 {
     private MathProcessor mp;
+    private List<string> Container;
     public MainPage()
 	{
 		InitializeComponent();
         mp = new MathProcessor();
+        Container = new List<string>();
     }
 
     #region Behavior Events
@@ -36,10 +38,12 @@ public partial class MainPage : ContentPage
         if (inputHasValue && input.Text != "0")
         {
             input.Text += btn.Text;
+            Container.Add(btn.Text);
         }
         else
         {
             input.Text = btn.Text;
+            Container.Add(btn.Text);
         }
     }
     private void OnOperatorButtonClicked(object sender, EventArgs e)
@@ -49,24 +53,28 @@ public partial class MainPage : ContentPage
         string lastCharacter = input.Text[input.Text.Length - 1].ToString();
         if (!mp.IsMathOperator(lastCharacter))
         {
-            input.Text += (sender as Button).ClassId;
+            string op = (sender as Button).ClassId;
+            input.Text += op;
+            Container.Add(op);
         }
     }
     private void OnResetClicked(object sender, EventArgs e)
     {
         input.Text = "0";
         rslt.Text = "0";
+        Container = new List<string>();
     }
 
     private void OnDeleteButtonClicked(object sender, EventArgs e)
     {
-        if(input.Text.Length <= 1)
+        if (input.Text.Length <= 1)
         {
             input.Text = "0";
             return;
         }
 
         input.Text = input.Text.Remove(input.Text.Length - 1);
+        Container.RemoveAt(Container.Count - 1);
     }
 }
 
